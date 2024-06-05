25edbc28942fd09b1318a2f02d54c2dc7672c44c
Hossein <hossein16akbari@gmail.com>
2024-06-05 23:47:44 +0330

    Creating store urls.py

--- ocp/urls.py
+++ ocp/urls.py
@@ -20,6 +20,9 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+admin.site.site_header = 'Online Course Provider Admin'
+admin.site.index_title = 'Admin'
+
 schema_view = get_schema_view(
    openapi.Info(
       title="My API",
@@ -35,7 +38,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api/', include('myapp.urls')),
+    path('store/', include('store.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('auth/', include('djoser.urls')),
