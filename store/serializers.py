cfbaa52319a34f60955e76605fe8c8d02ade661b
Hossein <hossein16akbari@gmail.com>
2024-06-06 00:17:10 +0330

    store serializers modification

--- store/serializers.py
+++ store/serializers.py
@@ -1,70 +1,50 @@
-from decimal import Decimal
 from django.db import transaction
 from rest_framework import serializers
 from .signals import order_created
-from .models import Cart, CartItem, Customer, Order, OrderItem, Product, Collection, Review, ProductImage
+from .models import Cart, CartItem, Customer, Order, OrderItem, Product, Collection, Review, ProductEpisode
 
 
 class CollectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Collection
-        fields = ['id', 'title', 'products_count']
+        fields = ['id', 'title', 'description', 'products_count']
 
     products_count = serializers.IntegerField(read_only=True)
     
     
-class ProductImageSerializer(serializers.ModelSerializer):
+class ProductEpisodeSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         product_id = self.context['product_id']
-        return ProductImage.objects.create(product_id=product_id,
+        return ProductEpisode.objects.create(product_id=product_id,
                                            **validated_data)
     
     class Meta:
-        model = ProductImage
-        fields = ['id', 'image']
+        model = ProductEpisode
+        fields = ['id', 'episode_number', 'file']
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    images = ProductImageSerializer(many=True, read_only=True)
+    product_episodes = ProductEpisodeSerializer(many=True, read_only=True)
     
     class Meta:
         model = Product
-        fields = ['id', 'title', 'description', 'slug', 'inventory',
-                  'unit_price', 'price_with_tax', 'collection', 'images']
-
-    price_with_tax = serializers.SerializerMethodField(
-        method_name='calculate_tax')
-
-    def calculate_tax(self, product: Product):
-        return product.unit_price * Decimal(1.1)
-
-
-class ReviewSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Review
-        fields = ['id', 'date', 'name', 'description']
-
-    def create(self, validated_data):
-        product_id = self.context['product_id']
-        return Review.objects.create(product_id=product_id, **validated_data)
+        fields = ['id', 'title', 'description', 
+                  'price', 'collection', 'product_episodes',
+                  'last_update', 'created_at']
 
 
 class SimpleProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'title', 'unit_price']
+        fields = ['id', 'title', 'price']
 
 
 class CartItemSerializer(serializers.ModelSerializer):
     product = SimpleProductSerializer()
-    total_price = serializers.SerializerMethodField()
-
-    def get_total_price(self, cart_item: CartItem):
-        return cart_item.quantity * cart_item.product.unit_price
 
     class Meta:
         model = CartItem
-        fields = ['id', 'product', 'quantity', 'total_price']
+        fields = ['id', 'product']
 
 
 class CartSerializer(serializers.ModelSerializer):
@@ -73,7 +53,7 @@ class CartSerializer(serializers.ModelSerializer):
     total_price = serializers.SerializerMethodField()
 
     def get_total_price(self, cart):
-        return sum([item.quantity * item.product.unit_price for item in cart.items.all()])
+        return sum([item.product.price for item in cart.items.all()])
 
     class Meta:
         model = Cart
@@ -92,12 +72,10 @@ class AddCartItemSerializer(serializers.ModelSerializer):
     def save(self, **kwargs):
         cart_id = self.context['cart_id']
         product_id = self.validated_data['product_id']
-        quantity = self.validated_data['quantity']
 
         try:
             cart_item = CartItem.objects.get(
                 cart_id=cart_id, product_id=product_id)
-            cart_item.quantity += quantity
             cart_item.save()
             self.instance = cart_item
         except CartItem.DoesNotExist:
@@ -122,7 +100,7 @@ class CustomerSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Customer
-        fields = ['id', 'user_id', 'phone', 'birth_date', 'membership']
+        fields = ['id', 'user_id', 'phone', 'birth_date']
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
