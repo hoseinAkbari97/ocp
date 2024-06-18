97891070bb16b99c093ca972eeab3eaa0b1b0951
Hossein <hossein16akbari@gmail.com>
2024-06-18 12:13:47 +0330

    adjusting tokens

--- ocp/settings.py
+++ ocp/settings.py
@@ -162,12 +162,14 @@ AUTH_USER_MODEL = 'userauths.CustomUser'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    )
 }
 
 
 SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
+   'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+   'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+   'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
 }
